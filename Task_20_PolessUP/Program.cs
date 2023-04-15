037865cfdc24c9d9a6156567143cd5023df3d967
CyberSanyok <sashad92175@gmail.com>
2023-04-15 23:00:54 +0300

    Добавьте файлы проекта.

---
+++ Task_20_PolessUP/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 12;// 2,3,5,7,11,13,17,19,23,29,31 ...
+        Console.WriteLine(GetList(n));
+    }
+    public static int GetList(int n)
+    {
+        int count = 1;
+        for (int i = 3; i <=n; i++)
+        {
+            for (int j = 2; j <i; j++)
+            {
+                if (i % j == 0) break;
+                if(j==i-1) count++;
+            }
+        }
+        return count;
+    }
+}
